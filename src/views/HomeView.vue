23cc228cf7fa71eabb524aad1b1ca0c7ee462944
melpualon <mel@xperto.ph>
2022-03-14 13:41:47 +0800

    uploaded credentialing-app frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Credentialing App API</h1>
+    <div>
+      <input type="text" v-model="name">
+      <input type="text" v-model="course">
+      <button @click="addCertificate">Add Certificate</button>
+    </div>
+
+    <div v-for="certificate in certificates" :key="certificate.id">
+      <h2>Name: {{ certificate.name }}</h2>
+      <p>Course: {{ certificate.course }}</p>
+      <button @click="deleteCertificate(certificate.id)">Delete Certificate</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      name: '',
+      course: '',
+      certificates: [
+        { name: 'Mel Doe', course: 'Engineering', id: 1 },
+        { name: 'Jane Fox', course: 'Accountancy', id: 2 },
+        { name: 'Chris Don', course: 'Civil Engineer', id: 3 },
+      ]
+    }
+  },
+  methods: {
+    deleteCertificate(id) {
+      this.certificates = this.certificates.filter(item => item.id !== id)
+    },
+    addCertificate() {
+      if(!this.name || !this.course) {
+        console.log('Please fill in all fields')
+        return
+      }
+
+      this.certificates.push({
+        name: this.name,
+        course: this.course,
+        id: this.certificates.length + 1
+      })
+    }
   }
 }
 </script>
